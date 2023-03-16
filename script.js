7d579eea1af5b04b36e299521a4b7a218b573be9
hari0082 <2430680@rts-365.dk>
2023-03-16 11:03:52 +0100

    add detail and more

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// lav globale variabler her som du kan genbruge.
 
 const listContainer = document.getElementById("list-container");
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+// nu kan jeg se alle 151 pokémons i konsol.
+/* Skriv (.results[0]) for at se første pokémon */
