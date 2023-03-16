e744771acee4c354ebb2fee98f864dfaad3a278c
hari0082 <2430680@rts-365.dk>
2023-03-16 13:47:48 +0100

    add layot + logo + js

--- script.js
+++ script.js
@@ -3,6 +3,22 @@
 const listContainer = document.getElementById("list-container");
 fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
   .then((response) => response.json())
-  .then((data) => console.log(data));
-// nu kan jeg se alle 151 pokémons i konsol.
-/* Skriv (.results[0]) for at se første pokémon */
+  .then((data) => {
+    data.results.forEach((pokemon) => {
+      fetch(pokemon.url)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          listContainer.innerHTML += `
+            <li>
+            <a href="detail.html?name=${data.name}" class="list-item">
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            ${data.name}
+            </a>
+            </li>
+            `;
+        });
+    });
+  });
