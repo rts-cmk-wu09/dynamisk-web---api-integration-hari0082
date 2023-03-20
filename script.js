cb39e53679d334521a10e08e81b777e43c4a07c3
hari0082 <2430680@rts-365.dk>
2023-03-20 14:16:06 +0100

    add axios and import

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+import axios from "./node_modules/axios/dist/esm/axios.js";
+
+const listContainer = document.getElementById("list-container");
+
+axios.get("https://pokeapi.co/api/v2/pokemon?limit=9")
+.then((response) => {
+  response.data.results.forEach((pokemon) => {
+    fetch(pokemon.url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        listContainer.innerHTML += `
+            <li>
+            <a href="detail.html?name=${data.name}" class="list-item">
+            <img src="${data.sprites.front_default}" alt="${data.name}">
+            ${data.name}
+            </a>
+            </li>
+            `;
+      });
+  });
+});
+
 // lav globale variabler her som du kan genbruge.
+
+/* 
 
 const listContainer = document.getElementById("list-container");
 fetch("https://pokeapi.co/api/v2/pokemon?limit=9")
@@ -21,4 +48,6 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=9")
             `;
         });
     });
-  });
+  }); 
+  
+  */
