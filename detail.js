929e5d20788b69c77c2b8095e23b19b3205e810b
hari0082 <2430680@rts-365.dk>
2023-03-21 10:54:51 +0100

    add detail site

--- detail.js
+++ detail.js
@@ -1,106 +1,7 @@
 // lav detajle side med hver pokemon
+
 // array af pokemons
-const pokemonArray = [
-  {
-    id: 1,
-    name: "bulbasaur",
-    types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
-    height: 7,
-    weight: 69,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-    },
-  },
-  {
-    id: 2,
-    name: "ivysaur",
-    types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
-    height: 10,
-    weight: 130,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-    },
-  },
-  {
-    id: 3,
-    name: "venusaur",
-    types: [{ type: { name: "grass" } }, { type: { name: "poison" } }],
-    height: 20,
-    weight: 1000,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png",
-    },
-  },
-  {
-    id: 4,
-    name: "charmander",
-    types: [{ type: { name: "fire" } }],
-    height: 6,
-    weight: 85,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
-    },
-  },
-  {
-    id: 5,
-    name: "charmeleon",
-    types: [{ type: { name: "fire" } }],
-    height: 11,
-    weight: 190,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
-    },
-  },
-  {
-    id: 6,
-    name: "charizard",
-    types: [{ type: { name: "fire" } }, { type: { name: "flying" } }],
-    height: 17,
-    weight: 905,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
-    },
-  },
-  {
-    id: 7,
-    name: "squirtle",
-    types: [{ type: { name: "water" } }],
-    height: 5,
-    weight: 90,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
-    },
-  },
-  {
-    id: 8,
-    name: "wartortle",
-    types: [{ type: { name: "water" } }],
-    height: 10,
-    weight: 225,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
-    },
-  },
-  {
-    id: 9,
-    name: "blastoise",
-    types: [{ type: { name: "water" } }],
-    height: 16,
-    weight: 855,
-    sprites: {
-      front_default:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
-    },
-  },
-];
+import axios from "./node_modules/axios/dist/esm/axios.js";
 
 // Gør dynamisk herunder:
 
@@ -109,16 +10,16 @@ const pokemon = params.get("name");
 
 const pokemonContainer = document.getElementById("pokemon-container");
 
-pokemonArray.forEach((pokemon) => {
+axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) => {
   pokemonContainer.innerHTML += `
     <div class="pokemon-card">
-      <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
-      <h2>${pokemon.name}</h2>
+      <img src="${response.data.sprites.front_default}" alt="${pokemon.name}">
+      <h2>${response.data.name}</h2>
       <ul>
-        <li>ID: ${pokemon.id}</li>
-        <li>Types: ${pokemon.types.map((type) => type.type.name)}</li>
-        <li>Height: ${pokemon.height}</li>
-        <li>Weight: ${pokemon.weight}</li>
+        <li>ID: ${response.data.id}</li>
+        <li>Types: ${response.data.types.map((type) => type.type.name)}</li>
+        <li>Height: ${response.data.height}</li>
+        <li>Weight: ${response.data.weight}</li>
       </ul>
     </div>
   `;
--- script.js
+++ script.js
@@ -2,24 +2,21 @@ import axios from "./node_modules/axios/dist/esm/axios.js";
 
 const listContainer = document.getElementById("list-container");
 
-axios.get("https://pokeapi.co/api/v2/pokemon?limit=9")
+axios.get("https://pokeapi.co/api/v2/pokemon?limit=151")
 .then((response) => {
   response.data.results.forEach((pokemon) => {
-    fetch(pokemon.url)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-        listContainer.innerHTML += `
+    axios.get(pokemon.url)
+    .then((response) => {
+        console.log(response)
+      listContainer.innerHTML += `
             <li>
-            <a href="detail.html?name=${data.name}" class="list-item">
-            <img src="${data.sprites.front_default}" alt="${data.name}">
-            ${data.name}
+            <a href="detail.html?name=${response.data.name}" class="list-item">
+            <img src="${response.data.sprites.front_default}" alt="${response.data.name}">
+            ${response.data.name}
             </a>
             </li>
             `;
-      });
+    });
   });
 });
 
